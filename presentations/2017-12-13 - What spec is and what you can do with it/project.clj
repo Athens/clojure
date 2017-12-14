4cbc4c22f78a6a1afb06597a30e58e3dff27c42d
Stathis Sideris <stathis@pixelated-noise.com>
2017-12-14 14:51:24 +0200

    Upload notes for talk on spec

---
+++ presentations/2017-12-13 - What spec is and what you can do with it/project.clj
@@ -0,0 +1,10 @@
+(defproject spec-demo "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/test.check "0.10.0-alpha2"]
+                 [net.cgrand/packed-printer "0.2.1"]
+                 [spec-provider "0.4.9"]
+                 [spectacles "0.3.3"]])
